4e6c621d881150815c006bfcc2cda1391e681671
lenin-nub <paullenin@hotmial.es>
2020-06-04 15:29:46 -0500

    Requerimientos1

---
+++ Requerimiento.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package requerimiento;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenin Iñacaza
+ */
+public class Requerimiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner objeto = new Scanner(System.in);
+        System.out.println("Implementación de un Multimodelo Neuronal Jerárquico para Identificación y Control de Sistemas Mecánicosddfghfgfgf");
+        System.out.println("\n               Requerimientos :");
+        System.out.println("\n Ingrese los requerimientos Funcionales  :");
+        String fun =objeto.nextLine();
+
+        System.out.println("\n Ingrese los requerimientos No Funcionales :");
+        String nofun =objeto.nextLine();
+
+        System.out.println("\n              Fase de Analisis :");
+        System.out.println("Ingrese objeto mundo real :");
+        String mund =objeto.nextLine();
+
+
+        System.out.println("\n              Fase de diseño:");
+        System.out.println("Ingrese Objeto Informatico :");
+        String carac7 =objeto.nextLine();
+        System.out.println("\n Atributos :");
+        System.out.println("\nIngrese Atributo 1 :");
+        String iatri =objeto.nextLine();
+        System.out.println("\nIngrese Atributo 2 :");
+        String iatri2 =objeto.nextLine();
+        System.out.println("\nIngrese Atributo 3 :");
+        String iatri3 =objeto.nextLine();
+        System.out.println("\n Servicios :");
+        System.out.println("\nIngrese Servicio 1 :");
+        String servi1 =objeto.nextLine();
+        System.out.println("\nIngrese Servicio 2 :");
+        String servi2 =objeto.nextLine();
+        System.out.println("\nIngrese Servicio 3 :");
+        String servi3 =objeto.nextLine();
+
+
+
+
+
+
+    }
+
+}
